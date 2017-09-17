2bea5189930f0c0485592328e1584c8f254e5ffb
Jason Lee <jason.khn.lee@gmail.com>
2017-09-16 22:00:54 -0400

    Item

--- app/src/main/java/nas509/groceries/model/Item.java
+++ app/src/main/java/nas509/groceries/model/Item.java
@@ -1,9 +1,25 @@
 package nas509.groceries.model;
 
+import java.math.BigDecimal;
+
 /**
  * Created by jason on 9/16/2017.
  */
 
 public class Item {
-    private
+    private String name;
+    private BigDecimal price;
+
+    public Item() {
+        this("No name", BigDecimal.ZERO);
+    }
+
+    public Item(String name, BigDecimal price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String toString() {
+        return this.name + price.toPlainString();
+    }
 }
