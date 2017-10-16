a473f09ac4fa71d4a3513860037fd62e74b32441
Rahul Tholakapalli <rahultholakapalli@gmail.com>
2017-10-15 20:42:29 -0400

    Added more login stuff

---
+++ app/src/main/java/nas509/groceries/controller/LoginActivity.java
@@ -0,0 +1,137 @@
+package nas509.groceries.controller;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import nas509.groceries.R;
+
+/**
+ * A login screen that offers login via username/password.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    /** UI references */
+    private EditText mUsernameView;
+    private EditText mPasswordView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        // Set up the login form.
+        //mUsernameView = (EditText) findViewById(R.id.username);
+        //mPasswordView = (EditText) findViewById(R.id.password);
+
+        // sets up password edit action listener
+//        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+//            @Override
+//            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+//                if (id == R.id.login || id == EditorInfo.IME_NULL) {
+//                    attemptLogin();
+//                    return true;
+//                }
+//                return false;
+//            }
+//        });
+
+        // setup login button listener
+        Button mLogInButton = (Button) findViewById(R.id.login_button);
+        mLogInButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogin();
+            }
+        });
+//
+        // setup cancel button listener
+        Button mCancelButton = (Button) findViewById(R.id.cancel_button);
+        mCancelButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cancel();
+            }
+        });
+
+    }
+
+    /** when cancel is pressed, closes activity */
+    private void cancel() {
+        finish();
+    }
+
+    /**
+     * Attempts to login the account specified by the login form.
+     * If there are form errors (invalid username, missing fields, etc.), the
+     * errors are presented and no actual login attempt is made.
+     */
+    private void attemptLogin() {
+
+//        // Reset errors.
+//        mUsernameView.setError(null);
+//        mPasswordView.setError(null);
+
+        // Store values at the time of the login attempt.
+//        String username = mUsernameView.getText().toString();
+//        String password = mPasswordView.getText().toString();
+//
+//        boolean cancel = false;
+//        View focusView = null;
+//
+//        // Check for a valid password, if the user entered one.
+//        if (!TextUtils.isEmpty(password) && !isPasswordValid(username, password)) {
+//            mPasswordView.setError(getString(R.string.error_incorrect_password));
+//            focusView = mPasswordView;
+//            cancel = true;
+//        }
+//
+//        // Check for a valid username address.
+//        if (TextUtils.isEmpty(username)) {
+//            mUsernameView.setError(getString(R.string.error_field_required));
+//            focusView = mUsernameView;
+//            cancel = true;
+//        }
+//
+//        if (cancel) {
+//            // There was an error; don't attempt login and focus the first
+//            // form field with an error.
+//            focusView.requestFocus();
+//        } else {
+            // Sets the currently logged in user and clears the activity stack
+//            UserManager userManager = UserManager.getInstance();
+//            userManager.setLoggedInUser(userManager.getUser(username));
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+            // hides the keyboard after login
+            //InputMethodManager inputManager = (InputMethodManager) this.getSystemService(Context.INPUT_METHOD_SERVICE);
+            //inputManager.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+
+            startActivity(intent);
+//        }
+    }
+
+    /**
+     * Checks that the username and password are a valid combination
+     * @param username      The username to check
+     * @param password      The password to check
+     * @return              True if valid, false if not
+     */
+//    private boolean isPasswordValid(String username, String password) {
+//        UserManager userManager = UserManager.getInstance();
+//        return userManager.checkUserCredentials(username, password);
+//    }
+}
+
